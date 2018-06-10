b5a771518eec3e312ad7358658d7c2430a2c4389
Joe Brennan <joe0brennan@gmail.com>
2018-06-10 16:57:23 +0100

    Initial commit
    Hello World

---
+++ 01_hello_world/main.py
@@ -0,0 +1,8 @@
+print('-----------------')
+print('     Hello!      ')
+print('-----------------')
+
+user_test = input('What is your name?\n')
+greeting = 'Nice to meet you ' + user_test
+
+print(greeting)
